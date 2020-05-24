cfe29285ce04aeb0579b9515f6a5c8da82632afd
luckysoo3516 <sumin328030@gmail.com>
2020-05-24 20:50:59 +0900

    Print A Month using Println
    Printed 4 weeks of a Month using Println.

--- src/darice/caladar/Calandar.java
+++ src/darice/caladar/Calandar.java
@@ -2,6 +2,11 @@ package darice.caladar;
 
 public class Calandar {
     public static void main(String[] args) {
-        System.out.println("Hello Caladar");
+        System.out.println("일 월 화 수 목 금 토\n" +
+                "--------------------\n" +
+                " 1  2  3  4  5  6  7\n" +
+                " 8  9 10 11 12 13 14\n" +
+                "15 16 17 18 19 20 21\n" +
+                "22 23 24 25 26 27 28");
     }
 }
